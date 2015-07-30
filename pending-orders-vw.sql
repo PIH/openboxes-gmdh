57eae59ca24c5044486526b0e3b49124cf61e479
jmiranda <jcm62@columbia.edu>
2015-07-29 21:12:31 -0500

    modified view definition because MySQL 5.5 does not support views with subqueries

--- pending-orders-vw.sql
+++ pending-orders-vw.sql
@@ -1,36 +1,37 @@
-CREATE OR REPLACE VIEW pending_orders_vw AS
-SELECT product_code, SUM(temp.quantity) FROM 
-(
-    SELECT 
-        product_code,
-        SUM(quantity) as quantity
-    FROM shipment_item
-    JOIN shipment ON shipment.id = shipment_item.shipment_id
-    JOIN shipment_status ON shipment.id = shipment_status.shipment_id
-    JOIN product ON shipment_item.product_id = product.id
-    WHERE (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Miami Warehouse' AND (shipment_status.status = 'Shipped' OR shipment_status.status IS NULL)) 
-    OR (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Log Center' AND (shipment_status.status = 'Shipped' OR shipment_status.status IS NULL)) 
-    OR (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND (shipment_status.status = 'Shipped' OR shipment_status.status IS NULL)) 
-    OR (shipment_status.origin = 'Miami Warehouse' AND shipment_status.destination = 'Log Center' AND shipment_status.status = 'Shipped') 
-    OR (shipment_status.origin = 'Miami Warehouse' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND shipment_status.status = 'Shipped') 
-    GROUP BY product.product_code
+CREATE OR REPLACE VIEW gmdh_inventory_vw AS
+SELECT product_id, location_id, MAX(date) AS maxdate
+FROM inventory_snapshot
+GROUP BY product_id, location_id;
 
-    UNION ALL 
+CREATE OR REPLACE VIEW gmdh_miami_inventory_vw AS
+SELECT
+  product.product_code as product_code,
+  GREATEST(inventory_snapshot.quantity_on_hand, 0) AS quantity
+FROM inventory_snapshot 
+INNER JOIN product ON inventory_snapshot .product_id = product.id
+INNER JOIN location ON inventory_snapshot .location_id = location.id
+INNER JOIN location_group ON location_group.id = location.location_group_id
+INNER JOIN location_type ON location_type.id = location.location_type_id
+INNER JOIN gmdh_inventory_vw AS is2 ON inventory_snapshot.location_id = is2.location_id AND inventory_snapshot.product_id = is2.product_id AND inventory_snapshot.date = maxdate
+WHERE location.name = 'Miami Warehouse'
+GROUP BY inventory_snapshot.product_id, inventory_snapshot.location_id;
 
-    SELECT
-      product.product_code as product_code,
-      GREATEST(inventory_snapshot.quantity_on_hand, 0) AS quantity
-    FROM inventory_snapshot 
-    INNER JOIN product ON inventory_snapshot .product_id = product.id
-    INNER JOIN location ON inventory_snapshot .location_id = location.id
-    INNER JOIN location_group ON location_group.id = location.location_group_id
-    INNER JOIN location_type ON location_type.id = location.location_type_id
-    INNER JOIN (
-        SELECT product_id, location_id, MAX(date) AS maxdate
-        FROM inventory_snapshot
-        GROUP BY product_id, location_id
-    ) AS is2 ON inventory_snapshot.location_id = is2.location_id AND inventory_snapshot.product_id = is2.product_id AND inventory_snapshot.date = maxdate
-    WHERE location.name = 'Miami Warehouse'
-    GROUP BY inventory_snapshot.product_id, inventory_snapshot.location_id
-) as temp
-GROUP BY temp.product_code;
+CREATE OR REPLACE VIEW gmdh_pending_orders_vw AS
+SELECT 
+    product_code,
+    SUM(quantity) as quantity
+FROM shipment_item
+JOIN shipment ON shipment.id = shipment_item.shipment_id
+JOIN shipment_status ON shipment.id = shipment_status.shipment_id
+JOIN product ON shipment_item.product_id = product.id
+WHERE (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Miami Warehouse' AND (shipment_status.status = 'Shipped' OR shipment_status.status IS NULL)) 
+OR (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Log Center' AND (shipment_status.status = 'Shipped' OR shipment_status.status IS NULL)) 
+OR (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND (shipment_status.status = 'Shipped' OR shipment_status.status IS NULL)) 
+OR (shipment_status.origin = 'Miami Warehouse' AND shipment_status.destination = 'Log Center' AND shipment_status.status = 'Shipped') 
+OR (shipment_status.origin = 'Miami Warehouse' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND shipment_status.status = 'Shipped') 
+GROUP BY product.product_code
+
+UNION ALL 
+
+SELECT product_code, quantity FROM gmdh_miami_inventory_vw;
+
