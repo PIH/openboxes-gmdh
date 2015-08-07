70533aedcf03e12e5e43464246caf71e66ff91af
jmiranda <jcm62@columbia.edu>
2015-08-07 16:47:13 -0500

    OBPIH-286 needed to add location_group alias to make sure we could join the view from the GMDH inventory query

--- pending-orders-vw.sql
+++ pending-orders-vw.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE VIEW pending_orders_vw AS
 
-  SELECT 'Mirebalais', product_code, quantity FROM hum_pending_orders_vw
+  SELECT 'Mirebalais' as location_group, product_code, quantity FROM hum_pending_orders_vw
 
   UNION ALL
 
-  SELECT 'Port au Prince', product_code, quantity FROM pap_pending_orders_vw
+  SELECT 'Port au Prince' as location_group, product_code, quantity FROM pap_pending_orders_vw
 
   UNION ALL
 
-  SELECT 'Sierra Leone', product_code, quantity FROM sl_pending_orders_vw
+  SELECT 'Sierra Leone' as location_group, product_code, quantity FROM sl_pending_orders_vw
