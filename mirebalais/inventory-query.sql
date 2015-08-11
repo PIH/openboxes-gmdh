370075a022440649a46ea540fe295578efbf6630
jmiranda <jcm62@columbia.edu>
2015-08-11 08:59:44 -0500

    OBPIH-315 removed reorder point from inventory query; added location classification to fix issue where we were including a consumption area (e.g. Famasi Ijans) in the QoH calculation

--- mirebalais/inventory-query.sql
+++ mirebalais/inventory-query.sql
@@ -14,18 +14,16 @@ FROM product
   INNER JOIN location_group
     ON (location_group.id = location.location_group_id
       AND location_group.name = 'Mirebalais')
-  INNER JOIN location_type
-    ON (location_type.id = location.location_type_id
-      AND location_type.name LIKE 'Depot%')
   INNER JOIN (
      SELECT product_id, location_id, MAX(date) AS maxdate
      FROM inventory_snapshot
      GROUP BY product_id, location_id
   ) AS is2 ON snap.location_id = is2.location_id AND snap.product_id = is2.product_id AND snap.date = maxdate
+  LEFT OUTER JOIN location_classification ON location_classification.location_id = location.id
   LEFT OUTER JOIN leadtime_summary_vw ON (product.product_code = leadtime_summary_vw.product_code AND leadtime_summary_vw.project = 'Haiti HUM Project')
-  LEFT OUTER JOIN reorder_point_vw ON (product.id = reorder_point_vw.product_id AND reorder_point_vw.location_group = 'Mirebalais')
   LEFT OUTER JOIN product_group_product ON product_group_product.product_id = product.id
   LEFT OUTER JOIN product_group ON product_group.id = product_group_product.product_group_id
   LEFT OUTER JOIN category product_group_category ON product_group_category.id = product_group.category_id
   LEFT OUTER JOIN pending_orders_vw ON (product.product_code = pending_orders_vw.product_code and pending_orders_vw.location_group = 'Mirebalais')
+WHERE location_classification.classification IN ('Level 2')
 GROUP BY snap.product_id;
