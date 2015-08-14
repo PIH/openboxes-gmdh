4a098420313361d0e49660230b9667ef8e403979
jmiranda <jcm62@columbia.edu>
2015-08-13 22:43:56 -0500

    slight improvement to the GMDH inventory query (shaves off about 2 seconds and improves readability)

--- port-au-prince/inventory-query.sql
+++ port-au-prince/inventory-query.sql
@@ -14,11 +14,6 @@ FROM product
   INNER JOIN location_group
     ON (location_group.id = location.location_group_id
       AND location_group.name = 'Port au Prince')
-  INNER JOIN (
-     SELECT product_id, location_id, MAX(date) AS maxdate
-     FROM inventory_snapshot
-     GROUP BY product_id, location_id
-  ) AS is2 ON snap.location_id = is2.location_id AND snap.product_id = is2.product_id AND snap.date = maxdate
   LEFT OUTER JOIN location_classification ON location_classification.location_id = location.id
   LEFT OUTER JOIN leadtime_summary_vw ON (product.product_code = leadtime_summary_vw.product_code AND leadtime_summary_vw.project = 'Haiti PAP Project')
   LEFT OUTER JOIN product_group_product ON product_group_product.product_id = product.id
@@ -26,4 +21,5 @@ FROM product
   LEFT OUTER JOIN category product_group_category ON product_group_category.id = product_group.category_id
   LEFT OUTER JOIN pending_orders_vw ON (product.product_code = pending_orders_vw.product_code and pending_orders_vw.location_group = 'Port au Prince')
 WHERE location_classification.classification IN ('Level 2')
+AND snap.date = (select max(date) from inventory_snapshot)
 GROUP BY snap.product_id;
