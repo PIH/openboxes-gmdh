4564140e0cd53f653fd0585bbc814d046daa1736
jmiranda <jcm62@columbia.edu>
2015-08-19 16:36:58 -0500

    OBPIH-316 updated location classifications according to the latest CSV

--- sierra-leone/update-location-classifications.sql
+++
@@ -1,2 +0,0 @@
-UPDATE location_classification set classification = 'Level 1' WHERE location_id = 'cab2b4f34d15cdfd014d20cc92ac467f';
-UPDATE location_classification set classification = 'Consumption Area' WHERE location_id = 'cab2b4f34d4f6086014d52873ca04038';
