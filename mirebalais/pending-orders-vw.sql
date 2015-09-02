a9043f827006138c0dfaf8e4c32afa2196e5eeb1
jmiranda <jcm62@columbia.edu>
2015-09-02 16:17:59 -0500

    OBPIH-286 updated pending orders view according to new logic in ticket

--- mirebalais/pending-orders-vw.sql
+++ mirebalais/pending-orders-vw.sql
@@ -1,11 +1,17 @@
 /**
+ *
  * Quantities shipped (not yet received) from vendor to HUM Depot 1
  * Quantities pending (not yet shipped) from vendor to HUM Depot 1
- * Quantities shipped (not yet received) from Boston to HUM Depot 1
+
+ * Quantities shipped (not yet received) from Boston HQ: Haiti Stock to HUM Depot 1
+ * Quantities shipped (not yet received) from Miami Warehouse: Haiti Stock to HUM Depot 1 (I don’t think we ever send things direct to HUM from Miami but just in case)
  * Quantities shipped (not yet received) from Log Center to HUM Depot 1
- * Quantities shipped (not yet received) from Miami to HUM Depot 1
- * I don’t think we ever send things direct to HUM from Miami but just in case
- * DO NOT include quantities pending (not yet shipped from Miami to HUM Depot 1
+
+ * Quantities pending (not yet shipped) from Bureau Fleriot PAP to HUM Depot 1
+ * Quantities shipped (not yet received) from Bureau Fleriot PAP to HUM Depot 1
+ *
+ * DO NOT include quantities pending (not yet shipped) from Miami to HUM Depot 1
+ * DO NOT include quantities pending (not yet shipped) from Boston to HUM Depot 1
  */
 CREATE OR REPLACE VIEW hum_pending_orders_vw AS
   SELECT
@@ -15,8 +21,13 @@ CREATE OR REPLACE VIEW hum_pending_orders_vw AS
   JOIN shipment ON shipment.id = shipment_item.shipment_id
   JOIN shipment_status ON shipment.id = shipment_status.shipment_id
   JOIN product ON shipment_item.product_id = product.id
-  WHERE (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND (shipment_status.status = 'Shipped' OR shipment_status.status IS NULL))
-  OR    (shipment_status.origin = 'Miami Warehouse' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND shipment_status.status = 'Shipped')
-  OR    (shipment_status.origin = 'Boston Headquarters' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND shipment_status.status = 'Shipped')
+  WHERE (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND shipment_status.status = 'Shipped')
+  OR    (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND shipment_status.status IS NULL)
+
+  OR    (shipment_status.origin = 'Boston HQ: Haiti Stock' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND shipment_status.status = 'Shipped')
+  OR    (shipment_status.origin = 'Miami Warehouse: Haiti Stock' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND shipment_status.status = 'Shipped')
   OR    (shipment_status.origin = 'Log Center' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND shipment_status.status = 'Shipped')
+
+  OR    (shipment_status.origin = 'Bureau Fleriot PAP' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND shipment_status.status = 'Shipped')
+  OR    (shipment_status.origin = 'Bureau Fleriot PAP' AND shipment_status.destination = 'HUM Depot 1 (Picking)' AND shipment_status.status IS NULL)
   GROUP BY product.product_code;
--- port-au-prince/pending-orders-vw.sql
+++ port-au-prince/pending-orders-vw.sql
@@ -1,25 +1,30 @@
 /**
+
  * Quantity on hand in Miami Warehouse
- * Quantities shipped (not yet received) from vendor to Miami Warehouse
- * Quantities pending (not yet shipped) from vendor to Miami Warehouse
+ * Quantity on hand in Boston HQ: Haiti Stock
+ * Quantities shipped (not yet received) from vendor to Miami Warehouse: Haiti Stock
+ * Quantities pending (not yet shipped) from vendor to Miami Warehouse: Haiti Stock
+ * Quantities shipped (not yet received) from vendor to Boston HQ: Haiti Stock
+ * Quantities pending (not yet shipped) from vendor to Boston HQ: Haiti Stock
  * Quantities shipped (not yet received) from vendor to Log Center
  * Quantities pending (not yet shipped) from vendor to Log Center
- * Quantities shipped (not yet received) from Miami to Log Center
- * Quantities shipped (not yet received) from Boston to Log Center
+ * Quantities shipped (not yet received) from Miami Warehouse: Haiti Stock to Log Center
+ * Quantities shipped (not yet received) from Boston HQ: Haiti Stock to Log Center
+ * Quantities shipped (not yet received) from Boston HQ: Haiti Stock to Miami Warehouse: Haiti Stock
  * DO NOT include quantities pending (not yet shipped) from Miami to Log Center
  */
 CREATE OR REPLACE VIEW miami_inventory_snapshot_vw AS
   SELECT
     product.product_code as product_code,
-    GREATEST(inventory_snapshot.quantity_on_hand, 0) AS quantity
+    GREATEST(SUM(inventory_snapshot.quantity_on_hand), 0) AS quantity
   FROM inventory_snapshot
   INNER JOIN product ON inventory_snapshot.product_id = product.id
   INNER JOIN location ON inventory_snapshot.location_id = location.id
   INNER JOIN location_group ON location_group.id = location.location_group_id
   INNER JOIN location_type ON location_type.id = location.location_type_id
   INNER JOIN latest_inventory_snapshot AS is2 ON inventory_snapshot.location_id = is2.location_id AND inventory_snapshot.product_id = is2.product_id AND inventory_snapshot.date = maxdate
-  WHERE location.name = 'Miami Warehouse'
-  GROUP BY inventory_snapshot.product_id, inventory_snapshot.location_id;
+  WHERE location.name IN ('Miami Warehouse: Haiti Stock', 'Boston HQ: Haiti Stock')
+  GROUP BY inventory_snapshot.product_id;
 
 CREATE OR REPLACE VIEW pap_pending_orders_vw AS
   SELECT
@@ -30,9 +35,14 @@ CREATE OR REPLACE VIEW pap_pending_orders_vw AS
   JOIN shipment_status ON shipment.id = shipment_status.shipment_id
   JOIN product ON shipment_item.product_id = product.id
   LEFT JOIN miami_inventory_snapshot_vw ON product.product_code = miami_inventory_snapshot_vw.product_code
-  WHERE (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Miami Warehouse' AND (shipment_status.status = 'Shipped' OR shipment_status.status IS NULL))
-  OR    (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Log Center' AND (shipment_status.status = 'Shipped' OR shipment_status.status IS NULL))
-  OR    (shipment_status.origin = 'Miami Warehouse' AND shipment_status.destination = 'Log Center' AND shipment_status.status = 'Shipped')
-  OR    (shipment_status.origin = 'Boston Headquarters' AND shipment_status.destination = 'Log Center' AND shipment_status.status = 'Shipped')
+  WHERE (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Miami Warehouse: Haiti Stock' AND shipment_status.status = 'Shipped')
+  OR    (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Miami Warehouse: Haiti Stock' AND shipment_status.status IS NULL)
+  OR    (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Boston HQ: Haiti Stock' AND shipment_status.status = 'Shipped')
+  OR    (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Boston HQ: Haiti Stock' AND shipment_status.status IS NULL)
+  OR    (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Log Center' AND shipment_status.status = 'Shipped')
+  OR    (shipment_status.origin_type = 'Supplier' AND shipment_status.destination = 'Log Center' AND shipment_status.status IS NULL)
+  OR    (shipment_status.origin = 'Miami Warehouse: Haiti Stock' AND shipment_status.destination = 'Log Center' AND shipment_status.status = 'Shipped')
+  OR    (shipment_status.origin = 'Boston Headquarters: Haiti Stock' AND shipment_status.destination = 'Log Center' AND shipment_status.status = 'Shipped')
+  OR    (shipment_status.origin = 'Boston Headquarters: Haiti Stock' AND shipment_status.destination = 'Miami Warehouse: Haiti Stock' AND shipment_status.status = 'Shipped')
   GROUP BY product.product_code;
 
