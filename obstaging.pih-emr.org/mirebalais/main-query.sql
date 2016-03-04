60aadb9adc02f5509d104cfbd651994b44f41677
jmiranda <jcm62@columbia.edu>
2016-03-04 08:20:10 -0600

    updated the main query for Mirebalais to use the canceled item quantity

--- obstaging.pih-emr.org/mirebalais/main-query.sql
+++ obstaging.pih-emr.org/mirebalais/main-query.sql
@@ -1,13 +1,31 @@
-SELECT
-  MAKEDATE(year, 1) + INTERVAL (vw.month - 1) MONTH AS stamp, 
-  SUM(quantity) AS SumOfquantity, 
-  CONCAT(category, '::', product_code) AS cat_code	
-FROM transaction_report_vw AS vw
-WHERE
-  source_hub = 'Mirebalais'
-  AND source_class IN ('Level 1', 'Level 2')
-  AND destination_hub IN ('Mirebalais')
-  AND destination_class = 'Consumption Area'
-GROUP BY year, month, product_code, 
-  category
-ORDER BY stamp
+SELECT stamp, sum(SumOfQuantity) as SumOfQuantity, cat_code
+FROM (
+	(
+	  SELECT
+      MAKEDATE(tvw.year, 1) + INTERVAL (tvw.month - 1) MONTH AS stamp,
+      SUM(tvw.quantity) AS SumOfquantity,
+      CONCAT(tvw.category, '::', tvw.product_code) AS cat_code,
+      'Issued'
+    FROM transaction_report_vw AS tvw
+    WHERE tvw.source_hub = 'Mirebalais'
+      AND tvw.source_class IN ('Level 1', 'Level 2')
+      AND tvw.destination_hub IN ('Mirebalais')
+      AND tvw.destination_class = 'Consumption Area'
+    GROUP BY tvw.year, tvw.month, tvw.product_code, tvw.category
+    ORDER BY stamp
+  )
+
+	UNION ALL
+
+	(
+	  SELECT
+      MAKEDATE(rvw.year, 1) + INTERVAL (rvw.month - 1) MONTH AS stamp,
+      SUM(rvw.total_missed) AS SumOfquantity,
+      CONCAT(rvw.category, '::', rvw.product_code) AS cat_code,
+      'Missed'
+    FROM requisition_report_vw AS rvw
+    GROUP BY rvw.year, rvw.month, rvw.product_code, rvw.category
+    ORDER BY stamp
+  )
+) as main_query 
+GROUP BY stamp, cat_code
